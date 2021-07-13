33efba108754724a39c3b085ba239290f0ebf9f4
Kayla <kay_kay1424@yahoo.com>
2021-07-13 14:00:02 -0500

    convert app to pwa

--- src/EventList.js
+++ src/EventList.js
@@ -2,18 +2,38 @@ import React, {Component} from 'react';
 import Event from './Event';
 import PropTypes from 'prop-types';
 
+import {InfoAlert} from './Alert';
+
 class EventList extends Component {
+    state = {
+        infoText: ''
+    }
+
+    componentDidMount() {
+        this.setState({
+            infoText: navigator.online 
+                ? '' : `The list loaded may not be up to date. 
+                Please go online to view the most current list.`
+        });
+    }
+
     render() {
         const {events} = this.props;
   
         return (
-            <ul className="Event-List">
-                {events.map(event =>
-                    <li key={event.id}>
-					  	<Event event={event} />
-                    </li>
-                )}
-            </ul>
+            <>
+                <InfoAlert 
+                    className="text-center"
+                    text={this.state.infoText} 
+                />
+                <ul className="Event-List">
+                    {events.map(event =>
+                        <li key={event.id}>
+                            <Event event={event} />
+                        </li>
+                    )}
+                </ul>
+            </>
         );
     }
 }
